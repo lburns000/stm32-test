c85afe5f58fee78ca255bf376a9aca3774fb40fb
name <name@example.com>
2021-12-22 12:34:29 -0600

    Added ssd1306 files

---
+++ gitTest/Core/Inc/ssd1306.h
@@ -0,0 +1,13 @@
+/*
+ * ssd1306.h
+ *
+ *  Created on: Dec 22, 2021
+ *      Author: luke
+ */
+
+#ifndef SSD1306_H_
+#define SSD1306_H_
+
+
+
+#endif /* SSD1306_H_ */
---
+++ gitTest/Core/Src/ssd1306.c
@@ -0,0 +1,8 @@
+/*
+ * ssd1306.c
+ *
+ *  Created on: Dec 22, 2021
+ *      Author: luke
+ */
+
+
